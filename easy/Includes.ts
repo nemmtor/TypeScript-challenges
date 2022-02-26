eda46e84e78f1c6ffec4e17856d38ba517924377
nemmtor <kacper0witas@gmail.com>
2022-02-26 17:53:05 +0100

    fix includes type

--- easy/Includes.ts
+++ easy/Includes.ts
@@ -1,6 +1,15 @@
-export type Includes<T extends any[], U> = U extends T[number] ? true : false;
+import { Equal } from "@type-challenges/utils";
 
-export type Input = ['a', 'b', 'c'];
+export type Includes<T extends any[], U> = T extends [
+  infer First,
+  ...infer Rest
+]
+  ? Equal<U, First> extends true
+    ? true
+    : Includes<Rest, U>
+  : false;
 
-export type Result = Includes<Input, 'a'>;
-export type Result2 = Includes<Input, 'd'>;
+export type Input = ["a", "b", "c"];
+
+export type Result = Includes<Input, "a">;
+export type Result2 = Includes<Input, "d">;
